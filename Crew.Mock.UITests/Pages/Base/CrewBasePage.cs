bd9651312fcfebc7b179a8b1ece0281cbbcf5b0d
Viacheslav Kliamchuk <vjatcheslav@clearion.com>
2021-08-11 16:45:38 +0000

    Merged in feature/UITests (pull request #1)
    added UITests
    
    * added UITests

---
+++ Crew.Mock.UITests/Pages/Base/CrewBasePage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crew.Mock.UITests.Pages.Base
+{
+    public abstract class CrewBasePage : BasePage
+    {
+        public T ChangeWiFiState<T>() where T : CrewBasePage
+        {
+            //TODO: Provide Backdoor methods on Native platforms
+
+            if (OnAndroid)
+            {
+                //var process = new System.Diagnostics.Process();
+                //var startInfo = new System.Diagnostics.ProcessStartInfo
+                //{
+                //    WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+                //    FileName = "cmd.exe",
+                //    Arguments = "/C adb shell am start -a android.intent.action.MAIN -n com.android.settings/.wifi.WifiSettings adb shell input keyevent 19 & adb shell input keyevent 19 & adb shell input keyevent 23 & adb shell input keyevent 82 & adb shell input tap 500 1000"
+                //};
+                //process.StartInfo = startInfo;
+                //process.Start();
+                throw new NotImplementedException();
+            }
+
+            if (OniOS)
+            {
+                throw new NotImplementedException();
+            }
+
+            return (T)this;
+        }
+    }
+}
---
+++ Crew.Mock.UITests/TESTS/Tests.cs
@@ -0,0 +1,17 @@
+﻿using Crew.Mock.UITests.Helpers;
+using Crew.Mock.UITests.Pages;
+using Crew.Mock.UITests.Tests.Base;
+using NUnit.Framework;
+using System.Collections.Generic;
+using Xamarin.UITest;
+
+namespace Crew.Mock.UITests.Tests
+{
+    public class Tests : CrewTestFixture
+    {
+        public Tests(Platform platform) : base(platform)
+        {
+
+        }
+    }
+}
